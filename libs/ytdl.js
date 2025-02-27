685e5718922615d747086b597c4587ddb0fba4c2
russellxz <162611099+russellxz@users.noreply.github.com>
2025-02-27 13:48:37 -0500

    Update ytdl.js

--- libs/ytdl.js
+++ libs/ytdl.js
@@ -1,26 +1,19 @@
 const axios = require("axios");
 
 async function ytdl(url) {
-    const apis = [
-        `https://api.dorratz.com/v2/yt-mp3?url=${url}`, 
-        `https://yt-downloader.sumanjay.workers.dev/audio?url=${url}`
-    ];
+const apis = [
+`https://ytdownloader.nvlgroup.my.id/audio?url=${url}&bitrate=128`,
+`https://api.dorratz.com/v2/yt-mp3?url=${url}`
+];
 
-    for (let api of apis) {
-        try {
-            console.log(`🔍 Probando API: ${api}`);
-            let response = await axios.get(api);
-            
-            // Verificar si la API devolvió correctamente la URL de descarga
-            if (response.data && response.data.link) {
-                return { status: "success", creador: "eliasaryt", dl: response.data.link };
-            }
-        } catch (e) {
-            console.error(`❌ Error en API: ${api} → ${e.message}`);
-        }
-    }
+for (let api of apis) {
+try {
+await axios.get(api);
+return { status: "success", creador: "eliasaryt", dl: api };
+} catch (e) {}
+}
 
-    return { status: "error", creador: "russell", dl: null };
+return { status: "error", creador: "eliasaryt", dl: null };
 }
 
 module.exports = ytdl;
