cb9fd2fadefdf4f0c05eeab6d1c6c699962d4702
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2025-03-15 16:43:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -214,7 +214,52 @@ sock.ev.on('messages.delete', (messages) => {
     });
 });
     switch (lowerCommand) {
-        
+        case 'gay': {
+    if (!msg.isGroup) {
+        return sock.sendMessage(msg.key.remoteJid, {
+            text: "⚠️ *Este comando solo funciona en grupos.*"
+        }, { quoted: msg });
+    }
+    const mentionedJid = msg.message.extendedTextMessage?.contextInfo?.mentionedJid?.[0];
+    if (!mentionedJid) {
+        return sock.sendMessage(msg.key.remoteJid, {
+            text: "⚠️ *Debes mencionar a un usuario.*\n\n📌 Ejemplo: `.gay @usuario`"
+        }, { quoted: msg });
+    }
+    await sock.sendMessage(msg.key.remoteJid, {
+        react: { text: '🏳‍🌈', key: msg.key }
+    });
+    const random = Math.floor(Math.random() * 100);
+    const gay = random;
+    let ga;
+    if (gay < 20) {
+        ga = 'Usted es hetero 🤪🤙';
+    } else if (gay >= 21 && gay <= 30) {
+        ga = 'Mas o menos 🤔';
+    } else if (gay >= 31 && gay <= 40) {
+        ga = 'Tengo mis dudas 😑';
+    } else if (gay >= 41 && gay <= 50) {
+        ga = 'Tengo razón? 😏';
+    } else if (gay == 50) {
+        ga = 'Eres o no? 🧐';
+    } else if (gay > 51) {
+        ga = 'Usted es gay 🥸';
+    }
+    const jawab = `@${mentionedJid.split("@")[0]} Es 🏳️‍🌈 ${random}% Gay\n\n${ga}`;
+    const imageUrl = `https://some-random-api.com/canvas/gay?avatar=${await sock.profilePictureUrl(mentionedJid, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')}`;
+    await sock.sendMessage(msg.key.remoteJid, {
+        image: { url: imageUrl },
+        caption: jawab,
+        mentions: [mentionedJid],
+        contextInfo: {
+            mentionedJid: [mentionedJid],
+            forwardingScore: 9999999,
+            isForwarded: false
+        }
+    }, { quoted: msg, ephemeralExpiration: 24 * 60 * 100, disappearingMessagesInChat: 24 * 60 * 100 });
+
+    break;
+}
 case 'toanime': {
     const fetch = require('node-fetch');
     try {
