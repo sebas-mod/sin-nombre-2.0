9997cd558b3630adc38d913166a981262f954ac9
russellxz <162611099+russellxz@users.noreply.github.com>
2025-03-28 08:15:37 -0400

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,6 +1,6 @@
 const handler = async (msg, { conn }) => {
   await conn.sendMessage(msg.key.remoteJid, {
-    text: "✅ El sistema de plugins está funcionando correctamente, Bolibot activo mi rey."
+    text: "✅ El sistema de plugins está funcionando correctamente, azura ultra 2.0 bot activo mi rey."
   }, { quoted: msg });
 };
 
