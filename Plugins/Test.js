f3fde774453674a5f69c9c50c2dbd2ea4b8d6a6b
russellxz <162611099+russellxz@users.noreply.github.com>
2025-03-28 07:34:53 -0400

    Delete Plugins directory

--- Plugins/Test.js
+++
@@ -1,8 +0,0 @@
-const handler = async (m, { conn, command, usedPrefix }) => {
-  await conn.sendMessage(m.chat, {
-    text: `✅ ¡Comando *${usedPrefix}${command}* ejecutado correctamente!\n\nTu bot está funcionando con el sistema *plugins* sin problemas.`
-  }, { quoted: m });
-};
-
-handler.command = ['test'];
-export default handler;
