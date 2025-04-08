a3b1cf4da573eb9b3a0a9cebe012ad4b5327e6ee
russellxz <162611099+russellxz@users.noreply.github.com>
2025-04-08 04:17:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -53,7 +53,17 @@ if (!fs.existsSync(welcomePath)) {
   fs.writeFileSync(welcomePath, JSON.stringify({}, null, 2));
   console.log("✅ Archivo welcome.json creado exitosamente.");
 }
+const setwelcome = require('./setwelcome');
 
+if (messageText.startsWith(global.prefix)) {
+  const command = messageText.slice(global.prefix.length).trim().split(" ")[0].toLowerCase();
+  const args = messageText.slice(global.prefix.length + command.length).trim();
+
+  if (command === "setwelcome") {
+    await setwelcome(msg, { conn: sock, text: args, usedPrefix: global.prefix });
+    return;
+  }
+}
 //grupo subot
 const rpgFile = "./rpg.json";
 if (!fs.existsSync(rpgFile)) {
