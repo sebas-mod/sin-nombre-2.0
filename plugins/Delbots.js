e74fb6985e05afa8ffd85e523de54bf4aedf0b92
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2025-04-11 19:34:41 -0400

    Update Delbots.js

--- plugins/Delbots.js
+++ plugins/Delbots.js
@@ -9,15 +9,12 @@ const handler = async (msg, { conn }) => {
   if (fs.existsSync(sessionPath)) {
     fs.rmSync(sessionPath, { recursive: true, force: true });
     await conn.sendMessage(msg.key.remoteJid, {
-      text: `🗑️ *Tu sesión ha sido eliminada correctamente.*\n\nPuedes volver a usar *#sercode o #code* cuando gustes.`,
-      quoted: msg
-    });
+      text: `🗑️ *Tu sesión ha sido eliminada correctamente.*\n\nPuedes volver a usar *#sercode o #code* cuando gustes.`}, {quoted: msg});
     console.log(`✅ Carpeta del subbot ${number} eliminada por comando.`);
   } else {
     await conn.sendMessage(msg.key.remoteJid, {
-      text: `⚠️ *No se encontró ninguna carpeta activa para eliminar.*`,
-      quoted: msg
-    });
+      text: `⚠️ *No se encontró ninguna carpeta activa para eliminar.*`
+    }, {quoted: msg});
   }
 };
 
