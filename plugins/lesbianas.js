9fb948451f9d6a9f05d089268a43c3e9d8f9c33f
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2025-04-21 10:46:51 -0400

    Create lesbianas.js

---
+++ plugins/lesbianas.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+
+const handler = async (msg, { conn, command }) => {
+  try {
+    const fallback = await axios.get("https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/imagenlesbians.json");
+    const api = await axios.get("https://api-fgmods.ddns.net/api/nsfw/lesbian?apikey=fg-dylux");
+
+    const url = api.data || fallback.data[Math.floor(Math.random() * fallback.data.length)];
+
+    await conn.sendMessage(msg.key.remoteJid, {
+      image: { url },
+      caption: `_${command}_`
+    }, { quoted: msg });
+  } catch (e) {
+    console.error("❌ Error en comando imagenlesbians:", e);
+   // await msg.reply("❌ No se pudo obtener la imagen.");
+  }
+};
+
+handler.command = ["imagenlesbians"];
+handler.tags = ["nsfw"];
+handler.help = ["imagenlesbians"];
+module.exports = handler;
