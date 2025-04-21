743ad4d9811ef45788c1e9a2299dd7fe4d51dd7a
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2025-04-21 11:12:05 -0400

    Update lésbico.js

--- plugins/lésbico.js
+++ plugins/lésbico.js
@@ -1,20 +1,20 @@
 const handler = async (msg, { conn }) => {
   const urls = [
-    'https://l.top4top.io/m_2257y4pyl0.mp4',
-    'https://k.top4top.io/m_2257pdwjy0.mp4',
-    'https://a.top4top.io/m_2257qulmx0.mp4',
-    'https://c.top4top.io/m_2257p4v9i3.mp4',
-    'https://j.top4top.io/m_2258joebc2.mp4'
+    'https://telegra.ph/file/4a270d9945ac46f42d95c.mp4',
+    'https://telegra.ph/file/f753759342337c4012b3f.mp4',
+    'https://telegra.ph/file/411d8f59a5cefc2a1d227.mp4',
+    'https://telegra.ph/file/76ba0dc2a07f491756377.mp4',
+    'https://telegra.ph/file/831bb88f562bef3f1a15d.mp4'
   ];
   const url = urls[Math.floor(Math.random() * urls.length)];
 
   await conn.sendMessage(msg.key.remoteJid, {
     video: { url },
-    caption: "👩‍❤️‍👩 Video lésbico +18"
+    caption: "🎥 Disfruta del video +18"
   }, { quoted: msg });
 };
 
-handler.command = ["videoxxxlesbi", "pornolesv"];
+handler.command = ["videoxxx", "vídeoxxx"];
 handler.tags = ["nsfw"];
-handler.help = ["videoxxxlesbi"];
+handler.help = ["videoxxx"];
 module.exports = handler;
